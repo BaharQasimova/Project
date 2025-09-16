9c9e74df401f12487bfa69d5300f0a33b99b4ec3
BaharQasimova <bahargsmff@gmail.com>
2025-09-16 19:25:41 +0400

    task

--- home/home.py
+++ home/home.py
@@ -7,4 +7,4 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0",port=6723)
+    app.run(debug=True, host="0.0.0.0",port=5733)
